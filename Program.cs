4a7ffa67838515c39adf1c86f39ca7eb9f714d23
rlatjswls14-crypto <rlatjswls14@gmail.com>
2026-02-24 20:53:54 +0900

    제출

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int result;
+
+Calculate(out result);
+
+Console.WriteLine($"결과: {result}");
+
+void Calculate(out int value)
+{
+    value = 1234;  // 반드시 값을 할당해야 함
+    Console.WriteLine($"메서드 내부: {value}");
+}
